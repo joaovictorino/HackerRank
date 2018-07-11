f41cf8a2b557d57bead7d97cd803c2fe2937320d
joaovictorino <jhvictorino@gmail.com>
2018-07-11 18:22:39 -0300

    python

--- python/03_condition.py
+++ python/03_condition.py
@@ -10,3 +10,7 @@ import sys
 
 if __name__ == '__main__':
     N = int(input())
+    if N%2 == 0 and ((N >= 2 and N <= 5) or (N > 20)):
+        print("Not Weird")
+    else:
+        print("Weird")
